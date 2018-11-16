96ca91c7160496f57676aed2eb6347fa1d2d43c7
ninapfister <1810653169@stud.fh-kufstein.ac.at>
2018-11-16 15:01:32 +0100

    Aufgabe4

---
+++ src/Aufgabe4_1810653169.java
@@ -0,0 +1,19 @@
+public class Aufgabe4_1810653169
+{
+    public static void main (String []args)
+    {
+        int zahl1 = 15;
+        int zahl2 = 20;
+
+        System.out.println(zahl1<zahl2);
+        System.out.println(zahl2<zahl1);
+        System.out.println(zahl1>zahl2);
+        System.out.println(zahl2>zahl1);
+        System.out.println(zahl1<=zahl2);
+        System.out.println(zahl2<=zahl1);
+        System.out.println(zahl1>=zahl2);
+        System.out.println(zahl2>=zahl1);
+        System.out.println(zahl1==zahl2);
+        System.out.println(zahl1!=zahl2);
+    }
+}
