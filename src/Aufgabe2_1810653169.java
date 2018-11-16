f8d060f38cdb47bb555a77c7372be2b436a137c0
ninapfister <1810653169@stud.fh-kufstein.ac.at>
2018-11-16 14:22:56 +0100

    Aufgabe2

---
+++ src/Aufgabe2_1810653169.java
@@ -0,0 +1,21 @@
+public class Aufgabe2_1810653169
+{
+        public static void main (String[]args)
+        {
+            String name = "Nina"; // Variable braucht 3 Attribute, also String mit name und meinen Namen in Anführungszeichen
+            int alter = 21; // int braucht eine Zahl, also alter und dann mein Alter, keine Anführungszeichen nötig weil Zahl, Zahlen zwischen -2147483648 und 2147483647 (32 Bit)
+            double kommastelle = 2.7; // mit double kann man eine Zahl mit Kommastelle angeben, Fließkommawert nach IEEE 754 zwischen ca. 4.9E-324 und 1.8E308 (Doppelte Genauigkeit, 64 Bit)
+            boolean wahrheitswert = true; // der Wahrheitswert wird berechnet, kann true oder false ergeben
+            long ganzeZahlen = -88756473; // ganze Zahlen zwischen -9223372036854775808 und 9223372036854775807 (64 Bit)
+            char einzelneZeichen = '\u0000'; // einzelne Zeichen des Unicode-Zeichensatzes zwischen '\u0000' und '\uffff' (16 Bit)
+            byte ganzeZahlen1 = 3; // ganze Zahlen zwischen -128 und 127 (8 Bit)
+            short ganzeZahlen2 = -26543; // ganze Zahlen zwischen -32768 und 32767 (16 Bit)
+            float fliesskommawert= 1.4E-45; // Fließkommawert nach IEEE 754 zwischen ca. 1.4E-45 und 3.4E38 (Einfache Genauigkeit, 32 Bit)
+
+            final int zahl = 218;
+            // Variable kann man ändern, hat 3 Attribute
+            // Konstante ist fix, kann man nicht ändern, final steht ganz vorne, hat 4 Attribute
+
+        }
+
+}
